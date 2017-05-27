6bb083bd0009349aeeb33ed592736e5c7368bd33
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2017-05-27 14:38:39 -0400

    Tune auto analyze/vacuum thresholds on ticks table

--- database.go
+++ database.go
@@ -7,4 +7,9 @@ import (
 func MigrateSchema() {
 	db := dbconf.DatabaseConnection()
 	db.AutoMigrate(&Tick{})
+
+	db.Exec("ALTER TABLE ticks SET (autovacuum_analyze_scale_factor = 0.0);")
+	db.Exec("ALTER TABLE ticks SET (autovacuum_analyze_threshold = 100000);")
+	db.Exec("ALTER TABLE ticks SET (autovacuum_vacuum_scale_factor = 0.0);")
+	db.Exec("ALTER TABLE ticks SET (autovacuum_vacuum_threshold = 100000);")
 }
